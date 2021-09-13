796f0efae982badc95b007b3067f2cff117e5628
fiosanabria <87140079+fiosanabria@users.noreply.github.com>
2021-09-13 13:33:35 -0300

    agregue el encabezado

---
+++ src/componet/Encabezado.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Encabezado = () => {
+  return (
+    <header>
+      <body>
+        <div>
+          {" "}
+          id="logo"
+          <img src="logo" alt="logo.png" />
+          <p>the rick and morty api</p>
+        </div>
+      </body>
+    </header>
+  );
+};
+
+export default Encabezado;
